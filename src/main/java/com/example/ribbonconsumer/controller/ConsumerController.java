0c15051914345e62098399ae0c2501c7500ec78d
weichao7573 <123456>
2017-12-20 17:29:50 +0800

    [A]初始化消费者工程

---
+++ src/main/java/com/example/ribbonconsumer/controller/ConsumerController.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.example.ribbonconsumer.controller;
+
+/**
+ * @title  ConsumerController
+ * @author Administrator
+ * @date   2017年12月20日下午4:02:55
+ */
+public class ConsumerController {
+
+}
